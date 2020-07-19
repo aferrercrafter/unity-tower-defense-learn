2d974134861e94dde5bd2cbb8097af75d5449483
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-07-19 13:18:22 -0300

    Added game manager empty object and wave spawner mechanic

---
+++ src/Assets/WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    public float countdown = 2f;
+
+    private int waveIndex = 0;
+    private int multiplier = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+            
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        Debug.Log("Wave Incomming");
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex * multiplier; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
