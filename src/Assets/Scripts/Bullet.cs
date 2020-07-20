77d1955d65cc2fa65692adb712f419aa6ffa683a
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-07-20 20:54:59 -0300

    Added bullets, shooting logic and effect

---
+++ src/Assets/Scripts/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 70f;
+    public float effectDuration = 2f;
+
+    public GameObject impactEffect;
+    Transform target;    
+
+
+    public void Seek (Transform _target)
+    {
+        target = _target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distancePerFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distancePerFrame)
+        {
+            HitTarget();            
+            return;
+        }
+
+        transform.Translate(dir.normalized * distancePerFrame, Space.World);
+        
+    }
+
+    void HitTarget()
+    {
+        GameObject effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, effectDuration);
+        Destroy(gameObject);
+        
+    }
+
+
+}
---
+++ src/Assets/Scripts/Turret.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    [Header("Attributes")]
+    public float range = 15f;
+    public float turnSpeed = 10f;
+    public float fireRate = 1f;
+
+    float fireCountdown = 0f;
+
+    [Header("Unity Configuration")]    
+    public Transform partToRotate;
+    public string enemyTag = "enemy";
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    Transform target;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0.0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject closestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (shortestDistance > distanceToEnemy)
+            {
+                shortestDistance = distanceToEnemy;
+                closestEnemy = enemy;
+            }
+        }
+
+        if (shortestDistance <= range)
+            target = closestEnemy.transform;
+        else
+            target = null;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        //Target Look on
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        
+        if(fireCountdown <= 0)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+        
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+        if(target != null)
+            bullet.Seek(target);
+    }
+
+    
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
