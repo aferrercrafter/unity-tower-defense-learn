30d03f3add2375ab17fa5dbc835d029440aadc4c
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-07-21 19:04:40 -0300

    added build standard turret mechanic

---
+++ src/Assets/Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+    private Color startColor;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't Build there! TODO UI Message");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+}
