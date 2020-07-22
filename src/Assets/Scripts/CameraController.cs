7324f454247a81dc187755933c5525f913a548a7
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-07-22 20:54:18 -0300

    added camera controller, need to fix to orthographicc view

---
+++ src/Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed = 30f;
+    public float panBorderThickness = 5f;
+
+    private bool doMovement = true;
+
+    Vector3 forward, right;
+
+    private void Start()
+    {
+        forward = transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            doMovement = !doMovement;
+
+        if (!doMovement)
+            return;
+        /*
+        if (Input.anyKey)
+            Move();
+        */
+
+        
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        transform.forward = heading;
+
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+}
